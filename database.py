2576b31fea4afdd41e5013a69c2b671f441e111d
J. Offenberg <jelmer.offenberg@gmail.com>
2024-11-20 17:26:00 +0100

    Update database.py with SQLModel changes

--- database.py
+++ database.py
@@ -1,27 +1,18 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlmodel import create_engine, SQLModel
 
-
-class Base(DeclarativeBase):
-    """This is the base class for all models in the application.
-
-    This is a pattern specific to SQLAlchemy and directly taken from the official documentation.
-    You must use this class as a base class for all your data models.
-    """
-    pass
-
-# Any tables that you would define, should be defined below this line.
+# Any tables that you would define, should be defined below this line using SQLModel.
 
 
 # This is the database engine that will be used to connect to the database.
-engine = create_engine("sqlite:///sqlite.db", echo=True)
+connect_args = {"check_same_thread": False}
+engine = create_engine("sqlite:///sqlite.db", echo=True, connect_args=connect_args)
 
 
 # This will create the tables in the database. Optionally, you can drop all tables before creating them.
-# Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+# SQLModel.metadata.drop_all(engine)
+SQLModel.metadata.create_all(engine)
 
 
 if __name__ == '__main__':
-    # This is a simple test to check if the database is working.
+    # Use this section to perform any testing of the database.py file.
     print("Hello, World!")
